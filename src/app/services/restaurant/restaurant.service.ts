0d608c2461e50a8429389f135f0b6d861bae2e62
nathangtg <22099865@imail.sunway.edu.my>
2024-05-10 18:51:30 +0800

    Initialised the Menu services and add 1 more endpoint to the restaurant service

--- src/app/services/restaurant/restaurant.service.ts
+++ src/app/services/restaurant/restaurant.service.ts
@@ -1,8 +1,23 @@
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RestaurantService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  apiUrl: string = environment.apiUrl;
+  restaurantId: number = 0; // Add a property to store the restaurant ID
+
+  getRestaurants() {
+    const url = `${this.apiUrl}/restaurants`;
+    return this.http.get(url);
+  }
+
+  getSpecificRestaurant(restaurantId: number) {
+    const url = `${this.apiUrl}/restaurants/${this.restaurantId}`;
+    return this.http.get(url);
+  }
 }
